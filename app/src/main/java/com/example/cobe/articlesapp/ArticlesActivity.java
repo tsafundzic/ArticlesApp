840750b8b6f7e677d1147beaf30dc33d4e76d7af
tsafundzic <tsafundzic@gmail.com>
2018-03-26 14:34:29 +0200

    Created Flaoting Action Button

--- app/src/main/java/com/example/cobe/articlesapp/ArticlesActivity.java
+++ app/src/main/java/com/example/cobe/articlesapp/ArticlesActivity.java
@@ -1,13 +1,22 @@
 package com.example.cobe.articlesapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ArticlesActivity extends AppCompatActivity {
 
+    FloatingActionButton floatingActionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles);
+
+        setUI();
+    }
+
+    private void setUI() {
+        floatingActionButton=findViewById(R.id.fabAddNewArticle);
     }
 }
